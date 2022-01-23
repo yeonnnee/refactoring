c5b61e23df867a301818ad3e0acefcf400b086ce
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2022-01-23 21:19:56 +0900

    amount 계산 함수 추출

--- refactoring1/index.js
+++ refactoring1/index.js
@@ -24,6 +24,30 @@ const plays = {
   othello:{name:"Othello", type:"tragedy"}
 }
 
+function getAmountForPlay(play, performance) {
+  let amount = 0;
+
+  switch (play.type) {
+    case "tragedy":
+      amount = 40000;
+      if (performance.audience > 30) {
+        amount += 1000 * (performance.audience - 30);
+      }
+      break;
+    case "comedy":
+      amount = 30000;
+      if (performance.audience > 20) {
+        amount += 1000 + 500 * (performance.audience - 20);
+      }
+      amount += 300 * performance.audience;
+      break;
+
+    default:
+      throw new Error(`알 수 없는 장르: ${play.type}`);
+  }
+  return amount;
+};
+
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumnCredits = 0;
@@ -34,26 +58,7 @@ function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playId];
-    let thisAmount = 0;
-
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 1000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
-    }
+    const amount = getAmountForPlay(play, perf);
 
     //포인트 적립
     volumnCredits += Math.max(perf.audience - 30, 0);
@@ -62,13 +67,16 @@ function statement(invoice, plays) {
     if ("comedy" === play.type) volumnCredits += Math.floor(perf.audience / 5);
 
     //청구 내역 출력
-    result += `${play.name} : ${format(thisAmount / 100)} (${perf.audience}석)\n`;
-    totalAmount += thisAmount;
+    result += `${play.name} : ${format(amount / 100)} (${perf.audience}석)\n`;
+    totalAmount += amount;
   }
 
   result += `총액: ${format(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumnCredits}점\n`;
   return result;
-}
+};
+
+
+
 
 statement(invoices[0], plays);
